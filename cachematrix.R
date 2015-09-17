028d78928c42a5febe81df6e04c57ca4cc960bb6
Ananda Mahto <mrdwab@gmail.com>
2015-09-17 16:55:33 +0530

    cosmetic changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,27 +1,33 @@
+# Overview of functionality provided --------------------------------------
+
 # Matrix inversion is usually a costly computation and there may be some
 # benefit to caching the inverse of a matrix rather than computing it
 # repeatedly. This pair of functions cache the inverse of a matrix.
 #
-# `makeCacheMatrix` creates a special "matrix" object that can cache its inverse.
-# `cacheSolve` computes the inverse of the special "matrix" returned by
-# `makeCacheMatrix`. If the inverse has already been calculated and the matrix
-# has not changed), then `cacheSolve` retrieves the inverse from the cache.
+# `makeCacheMatrix` creates a special "matrix" object that can cache
+# its inverse. `cacheSolve` computes the inverse of the special "matrix"
+# returned by `makeCacheMatrix`. If the inverse has already been
+# calculated and the matrix has not changed), then `cacheSolve`
+# retrieves the inverse from the cache.
 #
 # The matrix supplied must be invertible.
 
 
-# Creates a special "matrix" object that can cache its inverse.
+
+# The `makeCacheMatrix()` function ----------------------------------------
+
+# # Creates a special "matrix" object that can cache its inverse.
 #
-# Arguments:
+# # Arguments:
 #
-# x: a matrix that can be inverted
+# # x: a matrix that can be inverted
 #
-# Example:
+# # Example:
 #
 # hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
 # h8 <- makeCacheMatrix(hilbert(8))
 #
-# View the values:
+# # View the values:
 #
 # h8$get()
 
@@ -38,14 +44,16 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-# Computes the inverse of the special matrix created by `makeCacheMatrix()`
+# The `cacheSolve` function -----------------------------------------------
+
+# # Computes the inverse of the special matrix created by `makeCacheMatrix()`
 #
-# Arguments:
+# # Arguments:
 #
-#   x: A special matrix created using `makeCacheMatrix()`
-# ...: Other arguments to `solve()`
+# #   x: A special matrix created using `makeCacheMatrix()`
+# # ...: Other arguments to `solve()`
 #
-# Example:
+# # Example:
 #
 # hilbert <- function(n) { i <- 1:n; 1 / outer(i - 1, i, "+") }
 # h8 <- makeCacheMatrix(hilbert(8))
